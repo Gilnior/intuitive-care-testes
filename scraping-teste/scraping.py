a23c3655d78439577f24687671ee39bb987099fe
Gilnior <gilnior123@gmail.com>
2021-05-12 19:15:41 -0300

    sript working/home/gilnior/Projects/intuitive-care-testes/scraping-teste/env/bin/python

--- scraping-teste/scraping.py
+++ scraping-teste/scraping.py
@@ -1,9 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 
-source = requests.get('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar').text
-soup = BeautifulSoup(source, 'lxml')
 
-print(soup)
+def getpdfpagelink(debug=False):  # get the link to the page where the PDF is in
+    link = 'http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar'
+    source = requests.get(link).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    page_content = soup.find('div', class_='item-page')
+
+    ahref = page_content.a['href']
+
+    link_pdf_page = mountlink(ahref, link)  # the pdf link is in this page
+
+    return link_pdf_page
 
 
+def getpdflink():
+    link = getpdfpagelink()
+    source = requests.get(link).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    page_content = soup.find('div', class_='item-page')
+
+    ahref = page_content.tbody.a['href']
+
+    pdf_link = mountlink(ahref,link)
+
+    return pdf_link
+
+
+def mountlink(ahref,link):
+    homelink = link[:link.find('.br') + 3]
+    Link = f"{homelink}{ahref}"
+    return Link
+
+
+def main():
+    pdf_link = getpdflink()
+    r = requests.get(pdf_link, stream=True)
+    pdfname = pdf_link.split('/')[-1]
+
+    with open(f'{pdfname}', 'wb') as f:
+        f.write(r.content)
+    f.close
+    print(f"{pdfname} was sucessfully downloaded")
+
+
+if __name__=='__main__':
+    main()
